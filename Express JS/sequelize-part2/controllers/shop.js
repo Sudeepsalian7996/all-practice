4cbb3689c62861d29145bc9ff49abb0aa2afb2d1
Sudeepsalian7996 <sudeep7996>
2023-02-09 10:20:27 +0530

    sequelize all crud functionality

---
+++ Express JS/sequelize-part2/controllers/shop.js
@@ -0,0 +1,67 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll().then((product)=>{
+    res.render('shop/product-list', {
+      prods: product,
+      pageTitle: "all products",
+      path: '/products'
+    });
+  })
+  .catch((err)=>console.log("product error"))
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findByPk(prodId).then((product)=>{
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  })
+  .catch(()=>{
+    console.error("error in details button")
+  })
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll().then((product)=>{
+      res.render('shop/index', {
+        prods: product,
+        pageTitle: product.title,
+        path: '/'
+      });
+    })
+    .catch((err)=>console.log("shop error"))
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
