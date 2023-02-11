1d8a1236328b70946707a2bd127418a5221cfe50
Sudeepsalian7996 <sudeep7996>
2023-02-11 10:58:05 +0530

    one to many relation sequelize

---
+++ Express JS/one to many/models/product.js
@@ -0,0 +1,29 @@
+const Sequelize=require("sequelize")
+
+const sequelize=require("../util/database")
+
+const Products=sequelize.define("products",{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    primaryKey:true,
+    allowNull:false
+  },
+  title:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    allowNull:false,
+    type:Sequelize.STRING
+  }
+})
+module.exports=Products
